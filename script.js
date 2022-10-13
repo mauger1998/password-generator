d9c6ce738ff73f5b091bcca874dd457d93fe39cb
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-13 16:38:31 +0100

    Declare all arrays and write function to geneerate codes

--- script.js
+++ script.js
@@ -4,4 +4,45 @@ const uppercaseCheckbox = document.getElementById("includeUppercase");
 const numberCheckbox = document.getElementById("includeNumbers");
 const symbolCheckbox = document.getElementById("includeSymbols");
 const button = document.getElementById("button");
+const form = document.getElementById("passwordGenerator");
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+})
+
+function generatePassword(numberOfCharacters, includeUppercase, includeNumbers, includeSymbols) {
+    
+}
+
+function generateArray(high, low, arrayToPushTo) {
+    for (let i = low; i <= high; i++) {
+        arrayToPushTo.push(i);
+        
+    }
+}
+
+/*Arrays*/
+
+let arrayOfUppercaseCodes = [
+
+];
+
+let arrayOfLowercaseCodes = [
+    
+];
+
+let arrayOfSymbolCodes = [
+
+];
+
+let arrayOfNumberCodes = [
+
+];
+
+/* */
+
+generateArray(90, 65, arrayOfUppercaseCodes);
+generateArray(122, 97, arrayOfLowercaseCodes);
+generateArray(47, 33, arrayOfSymbolCodes);
+generateArray(57, 48, arrayOfNumberCodes);
