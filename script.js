190db19c757a91fcd4e60c9de7aff152607fe03f
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-13 16:22:38 +0100

    Add html to link range and number input

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const uppercaseCheckbox = document.getElementById("includeUppercase");
 const numberCheckbox = document.getElementById("includeNumbers");
 const symbolCheckbox = document.getElementById("includeSymbols");
 const button = document.getElementById("button");
+
