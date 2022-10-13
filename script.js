8323493668f064c32374db301d7ce57db8b2bc77
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-13 16:18:53 +0100

    Declare all variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const passwordOutput = document.querySelector("h3");
+const characterInput = document.querySelectorAll("#numberOfCharacters");
+const uppercaseCheckbox = document.getElementById("includeUppercase");
+const numberCheckbox = document.getElementById("includeNumbers");
+const symbolCheckbox = document.getElementById("includeSymbols");
+const button = document.getElementById("button");
